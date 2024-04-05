e1654dfa3d908791667f2ea92f07b58b91f2c7df
Vloon <pleunscholten@hotmail.com>
2024-04-05 13:21:29 +0200

    Fix documentation

--- helper_functions.py
+++ helper_functions.py
@@ -8,9 +8,9 @@ from jaxtyping import Array, Float, PRNGKeyArray
 
 def set_GPU(gpu:str = '') -> None:
     """
-    Sets the GPU safely in os.environ, and initializes JAX
-    PARAMS:
-    gpu : string format of the GPU used. Multiple GPUs can be seperated with commas, e.g. '0,1,2'.
+    Sets the GPU safely in os.environ
+    Args:
+        gpu: string format of the GPU used. Multiple GPUs can be seperated with commas, e.g. '0,1,2'.
     """
     if gpu is None: # If gpu is None, then all GPUs are used.
         gpu = ''
@@ -19,18 +19,20 @@ def set_GPU(gpu:str = '') -> None:
 def get_cmd_params(parameter_list:list) -> Dict:
     """
     Gets the parameters described in parameter_list from the command line.
-    Returns a dictionary containing the variables with their value, either the value defined in the command line or the default value.
-    PARAMS:
-    parameter_list : list of tuples containing (arg_name <str>, dest <str>, type <type>, default <any> [*OPTIONAL], nargs <str> [OPTIONAL])
-        - arg_name is the name of the argument in the command line.
-        - var_name is the name of the variable in the returned dictionary (which we re-use as variable name here).
-        - type is the data-type of the variable.
-        - default is the default value it takes if nothing is passed to the command line. This argument is only optional if type is bool, where the default is always False.
-        - nargs is the number of arguments, where '?' (default) is 1 argument, '+' concatenates all arguments to 1 list. This argument is optional.
+    Args:
+        parameter_list: list of tuples containing (arg_name <str>, dest <str>, type <type>, default <any> [*OPTIONAL], nargs <str> [OPTIONAL])
+            - arg_name is the name of the argument in the command line.
+            - var_name is the name of the variable in the returned dictionary (which we re-use as variable name here).
+            - type is the data-type of the variable.
+            - default is the default value it takes if nothing is passed to the command line. This argument is only optional if type is bool, where the default is always False.
+            - nargs is the number of arguments, where '?' (default) is 1 argument, '+' concatenates all arguments to 1 list. This argument is optional.
 
-        Example of a valid parameter list:
-            [('-m', 'mu', float, [1.,0.] '+'),
-            ('-s', 'sigma', float, 1.)]
+            Example of a valid parameter list:
+                [('-m', 'mu', float, [1.,0.] '+'),
+                ('-s', 'sigma', float, 1.)]
+
+    Returns:
+        cmd_params: dictionary containing the variables with their value, either the value defined in the command line or the default value.
     """
     ## Create parser
     parser = argparse.ArgumentParser()
@@ -53,15 +55,15 @@ def get_cmd_params(parameter_list:list) -> Dict:
     ## Parse arguments from CMD
     args = parser.parse_args()
     ## Create global parameters dictionary
-    global_params = {arg:getattr(args,arg) for arg in vars(args)}
-    return global_params
+    cmd_params = {arg:getattr(args,arg) for arg in vars(args)}
+    return cmd_params
 
 def write_seed(seed_file:str, key:PRNGKeyArray) -> None:
     """
     Writes the seed to the seed file
-    PARAMS:
-    seedfile : file location of the seed file
-    key : JAX random key
+    Args:
+        seed_file: file location of the seed file
+        key: JAX random key
     """
     old_seed = read_seed(seed_file)
     with open(seed_file, 'w') as f:
@@ -71,9 +73,11 @@ def write_seed(seed_file:str, key:PRNGKeyArray) -> None:
 def read_seed(seed_file:str) -> int:
     """
     Reads the seed from the seed file. The seed file consists of a line with the new seed and then a line with the old seed.
-    PARAMS:
-    seedfile : file location of the seed file
-    key : JAX random key
+    Args:
+        seed_file: file location of the seed file
+
+    Returns:
+        seed: the seed to be used for PRNGs
     """
     with open(seed_file, 'r') as f:
         next_seed = int(f.read().split('\n')[0])
