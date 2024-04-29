9720c45b492f5eed832d374c75f8ff3e051106ca
Vloon <pleunscholten@hotmail.com>
2024-04-29 15:10:05 +0200

    Add NUTS

--- convergence_test.py
+++ convergence_test.py
@@ -4,6 +4,7 @@ from helper_functions import get_cmd_params, set_GPU, read_seed, write_seed, tri
 
 arguments = [('-gpu', 'gpu', str, ''),
              ('-seed', 'seed', int, 1234),
+             ('-algo', 'algorithm', str, 'smc'),
              ]
 
 cmd_params = get_cmd_params(arguments)
@@ -26,21 +27,22 @@ from tensorflow_probability.substrates import jax as tfp
 tfd = tfp.distributions
 tfb = tfp.bijectors
 
-from blackjax import rmh
+from blackjax import rmh, nuts, window_adaptation
 from blackjax.diagnostics import potential_scale_reduction
 
 from typing import Dict, Callable, Tuple
 from jaxtyping import Array, Float
 from blackjax.smc.tempered import TemperedSMCState
 
-from models import ClusterModel, LSM, GibbsState, BayesianModel
+from models import LSM, GibbsState, BayesianModel
 
 ## Global parameters
 VERBOSE = True
 CONVERGENCE_FILENAME = 'convergence_log.txt'
+ALGO = cmd_param.get('algorithm')
 
 #
-def has_converged(samples, threshold:Float = 1.1, verbose:bool = False) -> bool:
+def has_converged(samples, threshold:Float = 1.1, verbose:bool = VERBOSE) -> bool:
     """
     Check whether the samples have converged, i.e. the potential scale reduction for all samples is smaller than the threshold
     Args:
@@ -57,7 +59,7 @@ def has_converged(samples, threshold:Float = 1.1, verbose:bool = False) -> bool:
     # however, those tend to be 'converged' anyway
     if verbose:
         # print([var for var in jax.tree_util.tree_leaves(R)])
-        log_txt = f"Maximum PSRF: {jnp.max(jnp.array([jnp.max(r) for r in jax.tree_util.tree_leaves(R)])):0.3f}"
+        log_txt = f"{ALGO}: Maximum PSRF: {jnp.max(jnp.array([jnp.max(r) for r in jax.tree_util.tree_leaves(R)])):0.3f}"
         print(log_txt)
         with open(CONVERGENCE_FILENAME, 'a') as f:
             f.write(log_txt)
@@ -120,13 +122,84 @@ def run_smc_to_convergence(key:PRNGKey, model:BayesianModel, num_mcmc:int, incre
         times.append(elapsed_in_attempt)
 
         if VERBOSE:
-            log_txt = f"{num_mcmc} MCMC steps took {elapsed_in_attempt} seconds to run."
+            log_txt = f"{ALGO}: iteration with {num_mcmc} MCMC steps took {elapsed_in_attempt} seconds to run."
             print(log_txt)
             with open(CONVERGENCE_FILENAME, 'a') as f:
                 f.write(log_txt)
                 f.write('\n')
 
     return samples, num_mcmc, times
+
+
+#
+def run_nuts_batch(key: PRNGKey, model: BayesianModel, num_mcmc: int, initial_state: GibbsState = None, nuts_parameters:Dict=None):
+    mcmc_parameters = dict(kernel=nuts,
+                           kernel_parameters=nuts_parameters,
+                           num_samples=num_mcmc,
+                           num_burn=0)
+    if initial_state is not None:
+        mcmc_parameters['initial_state'] = initial_state
+
+    samples = model.inference(key, mode='mcmc', sampling_parameters=mcmc_parameters)
+    return samples
+
+#
+def run_nuts_to_convergence(key, model, num_warmup, num_mcmc, increment, num_chains, burnin_factor=0.5, downsample_to=1000, psrf_threshold=1.1):
+    def init_fn_body(key, model):
+        return model.init_fn(key, num_particles=1).position
+
+    logdensity = lambda state: model.loglikelihood_fn()(state) + model.logprior_fn()(state)
+    warmup = window_adaptation(nuts, logdensity)
+
+    key, *subkeys = jrnd.split(key, num_chains + 1)
+    initial_states = jax.vmap(init_fn_body, in_axes=(0, None))(jnp.array(subkeys), model)
+
+    key, *subkeys = jrnd.split(key, num_chains + 1)
+    init_states_in_axes = jax.tree_map(lambda l: 0, initial_states)
+    (warm_states, warm_parameters), _ = jax.vmap(warmup.run, in_axes=(0,
+                                                                      init_states_in_axes,
+                                                                      None))(jnp.array(subkeys),
+                                                                             initial_states,
+                                                                             num_warmup)
+
+    warm_states_in_axes = jax.tree_map(lambda l: 0, warm_states)
+    warm_parameters_in_axes = jax.tree_map(lambda l: 0, warm_parameters)
+    key, *subkeys = jrnd.split(key, num_chains + 1)
+    samples = jax.vmap(run_nuts_batch, in_axes=(0,
+                                                None,
+                                                None,
+                                                warm_states_in_axes,
+                                                warm_parameters_in_axes))(jnp.array(subkeys),
+                                                                          model,
+                                                                          num_mcmc,
+                                                                          warm_states,
+                                                                          warm_parameters)
+
+    while not has_converged(samples, threshold=psrf_threshold, verbose=VERBOSE):
+        key, *subkeys = jrnd.split(key, num_chains + 1)
+        if VERBOSE:
+            log_txt = f"{ALGO}: Some parameters have not converged, now sampling at {num_mcmc+increment} samples"
+            print(log_txt)
+            with open(CONVERGENCE_FILENAME, 'a') as f:
+                f.write(log_txt)
+                f.write('\n')
+        final_states = jax.tree_map(lambda l: l[:, -1, ...], samples)
+        final_states_in_axes = jax.tree_map(lambda l: 0, final_states)
+        new_samples = jax.vmap(run_nuts_batch, in_axes=(0,
+                                                        None,
+                                                        None,
+                                                        final_states_in_axes,
+                                                        warm_parameters_in_axes))(jnp.array(subkeys),
+                                                                                  model,
+                                                                                  increment,
+                                                                                  final_states,
+                                                                                  warm_parameters)
+        samples = jax.tree_map(lambda *v: jnp.hstack(v), samples, new_samples)
+        num_mcmc += increment
+
+    samples = samples.position
+    samples = downsample(samples, burnin_factor=burnin_factor, downsample_to=downsample_to)
+    return samples, num_mcmc
 
 #
 def boxplot_coefficients(filename, samples, ylabel, labels=None):
@@ -191,7 +264,7 @@ hyperparams = dict(mu_z=0.,  # Mean of the _z Normal distribution
                    )
 gt_model = LSM(gt_priors, hyperparameters=hyperparams)
 
-key, prior_key, obs_key, smc_key = jrnd.split(key, 4)
+key, prior_key, obs_key, test_key = jrnd.split(key, 4)
 sampled_state = gt_model.sample_from_prior(prior_key, num_samples=1, max_distance=max_D)
 
 ## Also scale cluster means
@@ -210,30 +283,57 @@ nonh_con_prior = {latpos: dx.Normal(jnp.zeros((N-3, D)), jnp.ones((N-3, D))),
 
 learned_model = LSM(nonh_con_prior, observation)
 
-## Convergence test
 num_params = (N - 3) * D + 3 + 1
 num_particles = 1_000
 num_chains = 4
 psrf_threshold = 1.1
+burnin_factor = 0.5
 
-labels = None #??
+## Convergence test
+if ALGO == 'smc':
+    print('Starting SMC')
 
-samples_smc, num_mcmc, times_smc = run_smc_to_convergence(smc_key,
-                                                          learned_model,
-                                                          num_mcmc=1_000,
-                                                          increment=2,
-                                                          num_params=num_params,
-                                                          num_particles=num_particles,
-                                                          num_chains=num_chains,
-                                                          psrf_threshold=psrf_threshold
-                                                          )
+    labels = None #??
 
-if VERBOSE:
-    log_txt = f"Adaptive-tempered SMC done in {num_mcmc} steps per iteration ({times_smc[-1]:0.2f} seconds; {jnp.sum(jnp.array(times_smc)):0.2f} cumulative)"
-    print(log_txt)
-    with open(CONVERGENCE_FILENAME, 'a') as f:
-        f.write(log_txt)
-        f.write('\n')
+    samples_smc, num_mcmc, times_smc = run_smc_to_convergence(test_key,
+                                                              learned_model,
+                                                              num_mcmc=1_000,
+                                                              increment=2,
+                                                              num_params=num_params,
+                                                              num_particles=num_particles,
+                                                              num_chains=num_chains,
+                                                              psrf_threshold=psrf_threshold
+                                                              )
 
-boxplot_coefficients(f'figures/cluster_sim/convergence/smc_variable_selection_{latpos}.pdf', samples_smc.particles[latpos], r'$\beta$', labels)
-boxplot_coefficients(f'figures/cluster_sim/convergence/smc_variable_selection_lambda.pdf', samples_smc.particles['lam'], r'$\lambda$', labels)
+    if VERBOSE:
+        log_txt = f"Adaptive-tempered SMC done in {num_mcmc} steps per iteration ({times_smc[-1]:0.2f} seconds; {jnp.sum(jnp.array(times_smc)):0.2f} cumulative)"
+        print(log_txt)
+        with open(CONVERGENCE_FILENAME, 'a') as f:
+            f.write(log_txt)
+            f.write('\n')
+
+    boxplot_coefficients(f'figures/cluster_sim/convergence/smc_variable_selection_{latpos}.pdf', samples_smc.particles[latpos], latpos, labels)
+    boxplot_coefficients(f'figures/cluster_sim/convergence/smc_variable_selection_lambda.pdf', samples_smc.particles['lam'], r'$\lambda$', labels)
+elif ALGO == 'nuts':
+    print('Starting NUTS')
+
+    labels = None  # ??
+
+    start_nuts = time.time()
+    samples_nuts, num_mcmc = run_nuts_to_convergence(test_key,
+                                                     learned_model,
+                                                     num_warmup=1_000,
+                                                     num_mcmc=1_000,
+                                                     increment=1_000,
+                                                     num_chains=num_chains,
+                                                     burnin_factor=burnin_factor,
+                                                     downsample_to=num_particles,
+                                                     psrf_threshold=psrf_threshold)
+
+    end_nuts = time.time()
+    elapsed_nuts = end_nuts - start_nuts
+    print(f'Adaptive-tuned NUTS MCMC done in {num_mcmc} steps ({elapsed_nuts:0.2f} seconds)')
+
+    boxplot_coefficients(f'figures/cluster_sim/convergence/smc_variable_selection_{latpos}.pdf', samples_smc.particles[latpos], latpos, labels)
+    boxplot_coefficients(f'figures/cluster_sim/convergence/smc_variable_selection_lambda.pdf', samples_smc.particles['lam'], r'$\lambda$', labels)
+
